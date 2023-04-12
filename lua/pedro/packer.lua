4f06234a43222b03ee1adf066f3201d5951c5997
pedro barreiro <p.barreiro@kigroup.de>
2023-04-12 09:08:38 +0100

    fix: vertical stripe on screen

--- lua/pedro/packer.lua
+++ lua/pedro/packer.lua
@@ -7,14 +7,18 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-
-  vim.cmd('colorscheme rose-pine')
+  use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+          vim.cmd('colorscheme rose-pine')
+      end
+  })
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
